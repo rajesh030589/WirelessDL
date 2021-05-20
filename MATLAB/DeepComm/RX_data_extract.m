ae273e778758d403beb31748e9b874e72cbb6900
Rajesh <rajeshkmishra@utexas.edu>
2021-05-20 11:32:41 -0500

    basic working

--- MATLAB/DeepComm/RX_data_extract.m
+++ MATLAB/DeepComm/RX_data_extract.m
@@ -13,6 +13,9 @@ bit_per_symbol = log2(mod_order);
 total_no_bits = total_symbols*bit_per_symbol;
 encoded_no_bits = (total_no_bits - 12)/3;
 total_no_of_samples = no_of_ofdm_symbols*(size_of_FFT+ cp_length);
+no_of_pilot_carriers = 4;
+subcarrier_locations = [7:32 34:59];
+pilot_carriers = [12 26 40 54];
 
 % Extraction of the received data
 stringTX = strcat("TX.bin");
@@ -20,7 +23,7 @@ stringRX = strcat("RX.bin");
 
 TX = read_complex_binary(stringTX);
 RX = read_complex_binary(stringRX);
-
+% RX = [TX;TX;TX;TX;TX];
 k = 1;
 pow = zeros(length(RX),1);
 pkt_received = 0;
@@ -36,7 +39,7 @@ while length(RX) > total_symbols + 320
         k = k+1;
         continue;
     end
-        
+    
     
     % STS Packet Detection
     window_size = 16;
@@ -52,22 +55,25 @@ while length(RX) > total_symbols + 320
         end
         i = i + 1;
     end
-    st_id = i;
+    st_id = i +16;
     
     % LTS Symbol Alignment
     L = zeros(200,1);
-    lts = open('LTS.mat');
-    lts = lts.lts;
-    lts = lts(cp_length+1:cp_length + size_of_FFT);
+    
+    LTS = open('LTS.mat');
+    LTS = LTS.LTS;
+    lts = ifft(LTS(1:size_of_FFT,1),size_of_FFT);
+    
     for j = 1:200
         L(j) = sum(RX(st_id+j - 1:st_id+j - 1 +63).*conj(lts));
     end
+    
     [~,lt_id1] = max(L);
     L(lt_id1) = 0;
     [~,lt_id2] = max(L);
     lt_id = min(lt_id1,lt_id2);
     
-    lt_id = st_id + lt_id - 1 - 32;
+    lt_id = st_id + lt_id - 1 - 16;
     
     sts_start_id = lt_id-160;
     sts_end_id = lt_id - 1;
@@ -84,35 +90,67 @@ while length(RX) > total_symbols + 320
     
     % Packet Extraction
     pkt_received = pkt_received + 1;
-    packet.num = pkt_received;
-    packet.STS = RX(sts_start_id:sts_end_id);
-    packet.LTS1 = RX(lts1_start_id:lts1_end_id);
-    packet.LTS2 = RX(lts2_start_id:lts2_end_id);
-    packet.data = RX(data_start_id:data_end_id);
+    sts = RX(sts_start_id:sts_end_id);
+    lts1 = RX(lts1_start_id:lts1_end_id);
+    lts2 = RX(lts2_start_id:lts2_end_id);
+    y = RX(data_start_id:data_end_id);
     
     fprintf('Packet Receieved %d\n',pkt_received);
     
     % Coarse Frequency offset
-    sts = packet.STS;
     alpha = (1/16)*angle(sum(conj(sts(1:144)).*sts(17:160)));
     
-    lts1 = packet.LTS1;
-    lts1 = lts1.*exp(-1j.*[0:79]'*alpha);
-    
-    lts2 = packet.LTS2;
-    lts2 = lts2.*exp(-1j.*[80:159]'*alpha);     
-    
-    y = packet.data;
-    y = y.*exp(-1j.*[160:159+total_no_of_samples]'*alpha);
+    lts1 = lts1.*exp(-1j.*(0:79)'*alpha);
+    lts2 = lts2.*exp(-1j.*(80:159)'*alpha);
+    y = y.*exp(-1j.*(160:159+total_no_of_samples)'*alpha);
     
     fprintf('Frequency Corrected of Packet %d\n',pkt_received);
     
-    y = reshape(y, size_of_FFT_cp_length , no_of_ofdm_symbols);
-    Y = 
+    % Data Arranged
+    LTS1 = fftshift(fft(lts1(cp_length+1:size_of_FFT+cp_length,1)));
+    LTS2 = fftshift(fft(lts1(cp_length+1:size_of_FFT+cp_length,1)));
+    
+    y = reshape(y, size_of_FFT+cp_length , no_of_ofdm_symbols);
+    Y = zeros(size_of_FFT, no_of_ofdm_symbols);
+    for i = 1:no_of_ofdm_symbols
+        Y(:,i) = fftshift(fft(y(cp_length+1:size_of_FFT+cp_length,i)));
+    end
+    
+    % Channel Estimation
+    H = zeros(size_of_FFT,1);
+    for j = [7:32 34:58]
+        H(j)= 0.5*(LTS1(j)+LTS2(j))*sign(LTS(j));
+    end
+    
+    Pilots = open('Pilots.mat');
+    Pilots = Pilots.Pilots;
+    detected_symbols = zeros(total_symbols,1);
+    l = 1;
+    for i = 1:no_of_ofdm_symbols
+        theta = angle(sum(conj(Y(pilot_carriers,i)).*Pilots(:,i).*H(pilot_carriers,1)));
+        for j = subcarrier_locations
+            
+            detected_symbols(l,1) = (Y(j,i)/H(j))*exp(1j*theta);
+            l = l+1;
+        end
+    end
+    
+    
+        scatter(real(detected_symbols),imag(detected_symbols))
+    
+%     mod_symbols = open('mod_symbols.mat');
+%     mod_symbols = mod_symbols.mod_symbols;
+%     figure()
+%     hold on;
+%     
+%     one_bit = detected_symbols(mod_symbols==1);
+%     zero_bit = detected_symbols(mod_symbols==-1);
+%     scatter(real(one_bit),imag(one_bit),'b')
+%     scatter(real(zero_bit),imag(zero_bit),'r')
     
     RX(1:data_end_id) = [];
     
-    
+    break;
 end
 
 
--- MATLAB/DeepComm/TX_data_generate.m
+++ MATLAB/DeepComm/TX_data_generate.m
@@ -6,20 +6,24 @@ close all;
 no_of_ofdm_symbols = 800;
 size_of_FFT = 64;
 cp_length = 16;
-no_of_subcarriers = 51;
+no_of_subcarriers = 48;
 total_symbols = no_of_ofdm_symbols*no_of_subcarriers;
-mod_order = 2;
+mod_order = 4;
 bit_per_symbol = log2(mod_order);
 total_no_bits = total_symbols*bit_per_symbol;
 encoded_no_bits = (total_no_bits - 12)/3;
 
-
+total_no_of_samples = no_of_ofdm_symbols*(size_of_FFT+ cp_length);
+no_of_pilot_carriers = 4;
+subcarrier_locations = [7:32 34:59];
+pilot_carriers = [12 26 40 54];
+pilot_values = zeros(size_of_FFT,1);
+pilot_values(pilot_carriers,1) = [1;1;1;-1];
 
 % Encoding
 turboEnc = comm.TurboEncoder('InterleaverIndicesSource','Input port');
 dataIn = randi([0 1],encoded_no_bits,1);
 save('dataIn.mat', 'dataIn');
-
 intrlvrInd = randperm(encoded_no_bits);
 save('Interleaver.mat','intrlvrInd');
 
@@ -31,15 +35,26 @@ mod_symbols = qammod(encoded_data,mod_order,'InputType','bit','UnitAveragePower'
 save('mod_symbols.mat','mod_symbols');
 
 % Subcarrier Allocation
+P = open('Pilot_matrix.mat');
+P = P.PILOT;
+
+Pilots = zeros(no_of_pilot_carriers, no_of_ofdm_symbols);
 
 A = zeros(size_of_FFT, no_of_ofdm_symbols);
 k = 1;
 for i = 1:no_of_ofdm_symbols
-    for j = [7:32 34:58]
+    for j = subcarrier_locations
+        if any(pilot_carriers(:) == j)
+            A(j,i) = pilot_values(j,1)*P(1 + mod(i-1,127));
+        else
         A(j,i) = mod_symbols(k);
         k = k+1;
+        end
+        
     end
+    Pilots(:,i) = A(pilot_carriers,i);
 end
+save('Pilots.mat','Pilots');
 
 % IFFT to generate tx symbols
 for i = 1:no_of_ofdm_symbols
@@ -53,33 +68,25 @@ TX = TX.*(.8/(max(max(abs(real(TX))),max(abs(imag(TX))))));
 
 
 % Short Preamble Field
-STS =  [0, 0, 0, 0, 0, 0, 0, 0, (1.472+1.472j), 0, 0, 0, (-1.472-1.472j),...
-    0, 0, 0, (1.472+1.472j), 0, 0, 0, (-1.472-1.472j), 0, 0, 0, (-1.472-1.472j),...
-    0, 0, 0, (1.472+1.472j), 0, 0, 0, 0, 0, 0, 0, (-1.472-1.472j), 0, 0, 0, (-1.472-1.472j),...
-    0, 0, 0, (1.472+1.472j), 0, 0, 0, (1.472+1.472j), 0, 0, 0, (1.472+1.472j), 0, 0, 0, (1.472+1.472j),...
-    0, 0, 0, 0, 0, 0, 0];
-
-STS = STS.';
+STS = open('STS.mat');
+STS = STS.STS;
 IFFT_Data = ifft(STS(1:size_of_FFT,1),size_of_FFT);
-STS(1:size_of_FFT+cp_length,1) = [IFFT_Data(size_of_FFT - cp_length + 1: size_of_FFT);IFFT_Data];
-STS(1) = STS(1)*0.5;
-STS(size_of_FFT+cp_length) = STS(size_of_FFT+cp_length)*0.5;
-sts = [STS;STS];
+sts(1:size_of_FFT+cp_length,1) = [IFFT_Data(size_of_FFT - cp_length + 1: size_of_FFT);IFFT_Data];
+sts(1) = sts(1)*0.5;
+sts(size_of_FFT+cp_length) = sts(size_of_FFT+cp_length)*0.5;
+sts = [sts;sts];
 
 
 % Long Preamble Field
-LTS = [0,0,0,0,0,0,1,1,-1,-1,1,1,-1,1,-1,1,1,1,...
-    1,1,1,-1,-1,1,1,-1,1,-1,1,1,1,1,0,1,-1,-1,1,1,-1,...
-    1,-1,1,-1,-1,-1,-1,-1,1,1,-1,-1,1,-1,1,-1,1,1,...
-    1,1,0,0,0,0,0,];
-LTS = LTS.';
+LTS = open('LTS.mat');
+LTS = LTS.LTS;
 IFFT_Data = ifft(LTS(1:size_of_FFT,1),size_of_FFT);
-LTS(1:size_of_FFT+cp_length,1) = [IFFT_Data(size_of_FFT - cp_length + 1: size_of_FFT);IFFT_Data];
-LTS(1) = LTS(1)*0.5;
-LTS(size_of_FFT+cp_length) = LTS(size_of_FFT+cp_length)*0.5;
-lts = [LTS;LTS];
+lts(1:size_of_FFT+cp_length,1) = [IFFT_Data(size_of_FFT - cp_length + 1: size_of_FFT);IFFT_Data];
+lts(1) = lts(1)*0.5;
+lts(size_of_FFT+cp_length) = lts(size_of_FFT+cp_length)*0.5;
+lts = [lts;lts];
 
 TX = [sts;lts;TX];
 
-    
+
 write_complex_binary(TX,'TX.bin');
