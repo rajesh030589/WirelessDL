f24f16ef6b5b57cf928f976b4a2460152f654e83
Rajesh Mishra <rajeshkmishra@utexas.com>
2021-08-23 14:00:39 -0500

    made changes to encoder/decoder

--- Python/Automation/Imp_Functions/Encoder.m
+++ Python/Automation/Imp_Functions/Encoder.m
@@ -1,38 +1,40 @@
-function Encoded_data = Encoder(data_input, type, no_of_blocks, block_length, code_rate)
+function Decoded_data = Decoder(Demod_data, type, no_of_blocks, block_length)
 
     if strcmp(type, 'turbo')
-        turboEnc = comm.TurboEncoder('InterleaverIndicesSource', 'Input port');
-        coded_block_length = block_length / code_rate + 12;
-        Encoded_data = zeros(coded_block_length, no_of_blocks);
+        turboDec = comm.TurboDecoder('InterleaverIndicesSource', 'Input port', 'NumIterations', 6);
+        Interleaver = open('Interleaver.mat');
+        Interleaver = Interleaver.Interleaver;
+
+        Decoded_data = zeros(block_length, no_of_blocks);
 
         for i = 1:no_of_blocks
-            X = data_input(:, i);
-            intrlvrInd = [22, 20, 25, 4, 10, 15, 28, 11, 18, 29, 27,...
-                35, 37, 2, 39, 30, 34, 16, 36, 8, 13, 5, 17, 14, 33, 7,...
-                32, 1, 26, 12, 31, 24, 6, 23, 21, 19, 9, 38, 3, 0] + 1;
-            encoded_data = step(turboEnc, X, intrlvrInd);
-            Encoded_data(:, i) = encoded_data;
+            %             intrlvrInd = Interleaver(:, i);
+            intrlvrInd = [22, 20, 25, 4, 10, 15, 28, 11, 18, 29, 27, ...
+                        35, 37, 2, 39, 30, 34, 16, 36, 8, 13, 5, 17, 14, 33, 7, ...
+                        32, 1, 26, 12, 31, 24, 6, 23, 21, 19, 9, 38, 3, 0] + 1;
+            demod_data = Demod_data(:, i);
+            decoded_data = step(turboDec, demod_data, intrlvrInd);
+            Decoded_data(:, i) = decoded_data;
         end
-        
-    elseif strcmp(type, 'convolutional')
-        code_rate = 2; % Coding Rate
-        total_encoded_bits = code_rate * no_encoded_bits;
-        no_of_blocks = no_encoded_bits / block_length;
-        coded_block_length = code_rate * block_length;
 
-        encoded_data = zeros(total_encoded_bits, 1);
+    elseif strcmp(type, 'MAP')
+
         constraint_length = 3;
         TRELLIS = poly2trellis(constraint_length, [5 7], 7);
 
-        % Convolutional Encoder
-        hConEnc = comm.ConvolutionalEncoder('TrellisStructure', TRELLIS, 'TerminationMethod', 'Truncated');
+        % BCJR Decoder
+        hAPPDec = comm.APPDecoder('TrellisStructure', TRELLIS, 'TerminationMethod', 'Truncated', ...
+            'Algorithm', 'True APP', 'CodedBitLLROutputPort', true);
+
+        Decoded_data = zeros(block_length, no_of_blocks);
 
         for i = 1:no_of_blocks
-            st_id = (i - 1) * block_length;
-            data = data_input(st_id + 1:st_id + block_length);
-            encoded_data(2 * st_id + 1:2 * st_id + coded_block_length, 1) = hConEnc(data);
+            demod_data = Demod_data(:, i);
+            ll0 = zeros(block_length, 1);
+            llr = step(hAPPDec, ll0, demod_data);
+            data = (llr > 0); % MAP decoded bits
+            decoded_data = data(1:block_length);
+            Decoded_data(:, i) = decoded_data;
         end
 
     end
-
-end
